bb03dfe2dc0e98a93ad8590fe689e7560fe92c5b
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-23 07:03:48 +0530

    ADDED: greeting message

---
+++ Employee_Wage/src/com/bridgelabz/employeewagecomputation/EmpWageCalculation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class EmpWageCalculation {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation");
+
+	}
+
+}
