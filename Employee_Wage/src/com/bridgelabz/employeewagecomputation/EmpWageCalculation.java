3a0be9771d592b6400aa5c2eec109605a59c2abd
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-23 07:24:20 +0530

    ADDED: calculation of wage for a month

--- Employee_Wage/src/com/bridgelabz/employeewagecomputation/EmpWageCalculation.java
+++ Employee_Wage/src/com/bridgelabz/employeewagecomputation/EmpWageCalculation.java
@@ -5,11 +5,12 @@ public class EmpWageCalculation {
 		public static final int IS_PART_TIME = 1;
 		public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS =2;
+		public static final int MAX_HRS_IN_MONTH = 10;
 		public static void main(String[] args){
 		System.out.println("Welcome to employee wage computation");
-		int empHrs=0,totalEmpWage=0;
+		int empHrs=0,totalEmpHrs=0, totalWorkingDays=0;
 		int empWage=0;
-		for(int day=0;day < NUM_OF_WORKING_DAYS; day++){
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
 			int empCheck = (int)Math.floor(Math.random() * 10)%3;
 			switch(empCheck) {
 				case IS_FULL_TIME:
@@ -21,10 +22,10 @@ public class EmpWageCalculation {
 				default:
 					empHrs=0;
 				}
-			empWage= empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: "+empWage);
+			totalEmpHrs+=empHrs;
+			System.out.println("Day#: "+totalWorkingDays+"Emp Hr: "+empHrs);
 			}
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("total: " + totalEmpWage);
 
 	}
