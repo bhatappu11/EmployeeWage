e44dba66f9c5e3c3647d54aa52c6c840070ff215
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-27 14:19:00 +0530

    REFACTOR: employee wage for multiple companies

--- Employee_Wage/src/com/bridgelabz/employeewagecomputation/EmpWageCalculation.java
+++ Employee_Wage/src/com/bridgelabz/employeewagecomputation/EmpWageCalculation.java
@@ -4,20 +4,9 @@ public class EmpWageCalculation {
 		public static final int IS_FULL_TIME = 2;
 		public static final int IS_PART_TIME = 1;
 		
-		private final String companyName;
-		private final int empRatePerHour;
-		private final int numOfWorkingDays;
-		private final int maxHrsInMonth;
-		
-		public EmpWageCalculation(String companyName,int empRatePerHour,int numOfWorkignDays,int maxHrsInMonth) {
-			this.companyName = companyName;
-			this.empRatePerHour = empRatePerHour;
-			this.numOfWorkingDays = numOfWorkignDays;
-			this.maxHrsInMonth = maxHrsInMonth;
-		}
-		public static void computeEmpWage(EmpWageCalculation company) {
+		public static void computeEmpWage(String companyName,int empRatePerHour,int numOfWorkingDays,int maxHrsInMonth) {
 			int empHrs=0,totalEmpHrs=0, totalWorkingDays=0;
-			while(totalEmpHrs <= company.maxHrsInMonth && totalWorkingDays< company.numOfWorkingDays){
+			while(totalEmpHrs <= maxHrsInMonth && totalWorkingDays< numOfWorkingDays){
 				totalWorkingDays++;
 				int empCheck = (int)Math.floor(Math.random() * 10)%3;
 				switch(empCheck) {
@@ -33,16 +22,14 @@ public class EmpWageCalculation {
 				totalEmpHrs+=empHrs;
 				System.out.println("Day#: "+totalWorkingDays+" Emp Hr: "+empHrs);
 				}
-			int totalEmpWage=totalEmpHrs*company.empRatePerHour;
-			System.out.println("Company: "+company.companyName+" Total wage: " + totalEmpWage);
+			int totalEmpWage=totalEmpHrs*empRatePerHour;
+			System.out.println("Company: "+companyName+" Total wage: " + totalEmpWage);
 
 		}
 		public static void main(String[] args){
 		System.out.println("Welcome to employee wage computation");
-		EmpWageCalculation dMart = new EmpWageCalculation("D-Mart", 20, 10, 15);
-		EmpWageCalculation metro = new EmpWageCalculation("Metro", 25, 8, 12);
-		computeEmpWage(dMart);
-		computeEmpWage(metro);
+		computeEmpWage("dMart",20,4,10);
+		computeEmpWage("wallmart",10,4,20);
 	}
 
 }
